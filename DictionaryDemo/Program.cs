ef5beffe07a92201db86d6ea48f441203bf4d3af
Şerife Ünal <serife_unal.32@hotmail.com>
2021-01-23 19:24:37 +0300

    Add project files.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> myDictionary = new MyDictionary<string,string>();
+            myDictionary.Add("one" , "bir");
+            myDictionary.Add("two", "iki");
+            myDictionary.Add("three", "üç");
+            myDictionary.Add("four", "dört");
+            myDictionary.Add("five", "beş");
+
+            myDictionary.Read();
+           
+        }
+    }
+}
---
+++ MyDictonaryDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDictonaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
